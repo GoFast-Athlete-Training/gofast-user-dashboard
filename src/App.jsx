ecfd4793fb06b9c0957831684df469dcaf4dc793
adamgofast <adam@gofastcrushgoals.com>
2025-10-27 18:36:42 -0400

    Fix dashboard routing: Add _redirects file and remove ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,17 @@ import './index.css';
 import AdminHome from './pages/AdminHome';
 import AdminDashboardChoices from './pages/AdminDashboardChoices';
 import AdminAthletes from './pages/AdminAthletes';
-import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AdminHome />} />
-          <Route path="/admin" element={<ProtectedRoute><AdminDashboardChoices /></ProtectedRoute>} />
-          <Route path="/athlete-admin" element={<ProtectedRoute><AdminAthletes /></ProtectedRoute>} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminHome />} />
+        <Route path="/admin" element={<AdminDashboardChoices />} />
+        <Route path="/athlete-admin" element={<AdminAthletes />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
